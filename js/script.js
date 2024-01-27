9d953c7eae0385fca2b6b4852d22c274d6594274
Md. Fahim Hossain Sujon <120704636+FahimTS@users.noreply.github.com>
2024-01-27 16:14:52 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+
+    $('.back_to_top').click(() => {
+        $('html, body').animate({scrollTop: 0}, 500);
+    })
+
+    $(window).on('scroll', () => {
+        let scrollTop = $(this).scrollTop(); 
+
+        if(scrollTop > 200){
+            $('.navbar').addClass('main_menu_bg');
+        }else{
+            $('.navbar').removeClass('main_menu_bg');
+        }
+
+        if(scrollTop > 350){
+            $('.back_to_top').fadeIn()
+        }else{
+            $('.back_to_top').fadeOut()
+        }
+})
+})
+
+
